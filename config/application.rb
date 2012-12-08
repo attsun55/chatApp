0662104e817357a97b790f26df709f7da3ae9ec8
A.Morishita <a.morishita@gmail.com>
2012-12-09 00:06:59 +0900

    Herokuへのデプロイ用に設定を追加

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Sample
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
+ 
+    # deploying Rails 3.1 on Heroku
+    config.assets.initialize on precompile = false
+ end
 end
